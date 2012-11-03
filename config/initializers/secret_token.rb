4b678c79baba2e141e59732ca3512b5c8c88c13a
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2012-11-03 04:50:45 +0200

    FINALLY

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmaps::Application.config.secret_token = '61ae887aa813609d7cf69a2385ee8752088b996614f60a5ced9b54d8136ef566b527548af2b5ee907476bf78a810bfcdbc2aefaf7a8ce37823019491a8d80300'
